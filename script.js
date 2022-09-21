8c018ccd88c9d4a6e7285eec626622bf2956c79e
karthigasun <prkarthik3@gmail.com>
2022-09-21 16:10:12 +0530

    review

---
+++ script.js
@@ -0,0 +1,660 @@
+// var i=2
+// var b=1
+// var c=(i+b)
+// console.log(c
+//var s = ["i", "like", "this",
+	//	"program", "very", "much"];
+										
+//var ans ="";
+//for (var i = 5; i >= 0; i--)
+//{
+	//ans += s[i] + " ";
+//}
+//document.write(
+//"Reversed String:"+ "<br>");
+//document.write(
+//ans.slice(0,ans.length-1));
+
+
+//random_bg_color();
+//function small_large(grt){
+  //  t=-1;
+   // grt=grt.sort(function(a,b){return a-b})
+    //if(grt==[]){
+    //console.log(t);
+    //}
+    //else
+      //  console.log(grt[0]);
+
+//}
+
+//let a=[10,5,11,20,12],great=[];
+//for(i=0;i<a.length;i++){
+  //  for(j=0;j<a.length;j++){
+    //    if(a[i]<a[j]){
+      //      great.push(a[j]);
+       // }
+        
+   // }
+    //console.log(great);
+    //small_large(great);
+    //great=[];
+//}
+
+/* function age(){
+	let input=document.getElementById("change").value;
+	input=new Date(input);
+	input=input.getTime();
+	console.log(input);
+	let date2=Date.now();
+	console.log(date2);
+	let cage=date2-input;
+	cage=new Date(cage);
+	cage=cage.getFullYear();
+	cage=cage-1970;
+	console.log(cage);
+}
+document.getElementById("btn").addEventListener("click",age); */
+// var i=5
+
+// if(i<10){
+//     console.log(i)
+// }
+
+// var i=10
+// if (i>11){
+//     console.log(i)
+// }
+// else {
+//     console.log("iii")
+// }
+// let i=10
+// if (i<10){
+//     console.log("notok")
+// }
+// else{
+//     console.log("ok")
+// }
+
+// for(i=1; i<15; i++){
+//     if(i%2==0)
+//     {
+//         console.log(i)
+// }
+// else{
+//     console.log(i)
+// }
+//let d=17,mon=09,yr=2022;
+//date=mon+"/"+d+"/"+yr;
+//console.log(date);
+//let gn_date=new Date("09/17/2022"),
+//let gn_date=new Date(date);
+//console.log(gn_date);
+//day=gn_date.getDay(gn_date);
+//out=gn_date-((24*60*60*1000)*(day-1));
+//output=new Date.getTime(out);
+//console.log(getTime(out));
+// }
+// var i=1
+// while(i<20){
+//   i= +i;i++
+//   console.log(i)
+// }
+// let i=1
+// do{
+//     i=+i;i++;
+//     console.log(i)
+// }
+// while(i<10)
+
+// Area of circle
+// let r=50;
+// let a,bi;
+// bi=22/7;
+// a=bi*(r**2);
+// console.log(a);
+
+// Area of triangle
+// let  b=20;
+// let h=20;
+// let a;
+// a=0.5*(20*20)
+// console.log(a)
+
+// simple intrest 
+// let p=5;
+// let n=2;
+// let r=10
+// let s;
+// s=(p*n*r)/100
+// console.log(s)
+
+// Area of square
+// let a=10;
+// let area;
+// area=(a*a)
+// console.log(area)
+
+// Area of rectangle 
+// let b=10;
+// let h=5;
+// let a;
+// a=(b*h)
+// console.log(a)
+
+// Swap
+// let a=100;
+// let b=200;
+// console.log(a);
+// console.log(b);
+
+// c=a 
+// a=b
+// b=c 
+// console.log(a);
+// console.log(b);
+
+// let x=11
+
+// if (x<10){
+//     console.log("give")
+// }else if(x>12){ 
+//     console.log("take")
+// }else{
+//     console.log ("policy")
+// }
+
+
+
+// let a=11;
+
+// if (a<10){
+//     console.log("res");
+// }else if(a==10){
+//     console.log("red");
+// }else{
+//     console.log("abcd")}
+
+// let z=999;
+// let c="order";
+// function online(c){
+//     let d="kkk";
+//     if (z>1000){
+//         return(c);        
+//     }
+//     else if(z<1000){
+//         return(d);
+//     }
+// }console.log(online(c))
+
+
+// let x=36;
+// let y="mark";
+// function pass(y){
+//     if(x<35){
+//         return("fail")
+//     }
+//     else if(x>35){
+//         return("pass")
+//     }
+// }
+// console.log(pass(y))
+
+// let x0=10;
+// let x1=10;
+// let x2=10;
+// let x3=10;
+// let x4=10;
+// let x5=10;
+// let x6=10;
+
+// let x=[x0,x1,x2,x3,x4,x5,x6]
+
+// console.log(x);
+// let sum,s=0;
+// for(i=0;i<7;i++ ){
+//     sum=x[i]+s;
+//     s=sum;
+//     console.log(s);
+//  }
+//  let y=0
+
+// for(i=0;i<7;i++){
+//     z=x[i]+y;
+// }console.log(z);
+
+//let a=[];
+//let s,sum,avg,n;
+
+//s=0;
+
+//n=parseInt(prompt("value"));
+
+//for(i=0;i<n; i++){
+  //  a[i]=parseInt(prompt("value"))
+
+//    sum=a[i]+s;
+  //  s=sum
+
+//}
+
+//avg=(sum/n)
+//console.log(sum);
+
+//console.log(a);
+
+
+//console.log(avg);
+
+//let a=setTimeout("",5000);
+
+//console.log(a);
+
+//function alertm(){
+//alert ("Danger");}
+
+//SET TIME OUT () METHOD
+
+ //function delayedMessage(){
+	//setTimeout(() => {console.log("This is slow")},2000);
+//}
+	//getElementsByTagName("button");
+//clearTimeout(delayedMessage);
+
+
+  //setTimeout(() => {console.log("this is the first message")}, 5000);
+  //setTimeout(() => {console.log("this is the second message")}, 3000);
+  //setTimeout(() => {console.log("this is the third message")}, 1000);
+
+  // Output:
+
+  // this is the third message
+  // this is the second message
+  // this is the first message
+
+//let mark1 =parseInt(prompt("give  the value"));
+//let mark2 =parseInt(prompt("give  the value"));
+
+//if (mark1>mark2){
+//console.log("mark 1 is higher");}
+//else{
+//	console.log("mark 2 is higher");
+//}
+
+//let age = parseInt(prompt("give the age"));
+
+//if(age==18){
+//	if(age>=18){
+//	console.log("The age is =",age);}
+		
+//	else if(age<=21){
+//	console.log("The age is above 21",age);
+//}
+
+//}
+//else{
+//	console.log("The age is",age);
+//}
+
+//let a=prompt("Give Animal Name");
+
+//switch(a){
+	//case "lion":
+	//console.log("The Animal is",a);
+	//break;
+	//case "Tiger":
+	//console.log("The Animal is",a);
+	//break;
+	//case "deer":
+	//console.log("The Animal is",a); 
+	//break;
+	//default: console.log("U have not entered the animal name");
+//}
+
+//function clearMessage(){
+	//alert("Danger");
+	//let y=document.getElementById("alert").innerHTML="Touch";
+	
+//}
+
+//SET INTERVAL
+
+//let a=setInterval(clearMessage,4000);
+
+//function clearMessage(){
+	//alert ("This is Danger")
+//}
+
+//SET TIMEOUT
+//let a= setTimeout(fun,3000);
+
+//function fun(){
+	//alert("Welcome")
+//}
+
+//let b=clearTimeout(clear,6000);
+
+//function clear(){
+	//alert ("clear");
+//}
+
+
+
+//let age = parseInt(prompt("give  the value"));
+//let gen = prompt("m or f");
+//let mar = prompt("Give Marital Status");
+
+//if(gen=="f"){
+	//console.log("she works in urban areas");
+//}
+	
+
+//else if(gen=="m"){
+	//if((age>=20)&&(age<40)){
+		//console.log("he works anywhere");
+//}
+
+	//else if((age>40)&&(age<60)){
+		//console.log("He only works in urban areas");
+//}
+
+	//else{
+	//console.log("error");
+	//}
+//}
+
+
+//let n=parseInt(prompt("Give the Value")),i=n;
+
+//for(i=n;i>0;i--){
+	//console.log(i);
+//}
+//let a={brand:"lenovo",color:"black"};
+
+//for(let x in a){
+	//console.log(a[x]);
+//}
+
+
+//let
+//a=parseInt(prompt("Give the Value of Table")),s=0,b,i=0;
+//c=parseInt(prompt("Give the Value to Number")),c;
+
+//for(i=1;i<=c;i++){
+    
+   // b=(a*i);
+  //  console.log(i,"*",a,"=",b);
+//}
+
+//let n=parseInt(prompt("Give the Value")),i=1,s;
+
+//while(i<=n){
+	//s=(i*i*i);
+	//console.log(s);
+	//i++;
+//}
+
+  
+
+//function add(a,b){
+	//return(a+b);
+//}
+//let 
+//a=parseInt(prompt("Give the value")),
+//b=a=parseInt(prompt("Give the value")),c;
+
+//c=add(a,b);
+//console.log(c);
+
+//function odd(n){
+	//if(n%2==0){
+		//console.log("The Number is even",n);
+	//}
+	//else{
+	//	console.log("The Number is Odd",n);
+	//}
+//}
+
+//let n=parseInt(prompt("Give the value"));
+
+//odd(n);
+
+// let a="karthik",b;
+// console.log(a.length);
+
+// let a="abc,der,xyz";
+// console.log(a);
+// let b=console.log(a.substring(-8));
+
+// let a="PLEAse visit";
+// console.log(a);
+// let b=console.log(a.toLowerCase();
+//let a="karthik is a good boy";
+//let b=console.log(a.repaet(3));
+
+
+//let a="3";
+//console.log(a.padEnd(2,"k"));
+
+//color
+
+//function background() {
+  //  let
+	//a=Math.trunc(Math.min(20) * 256),
+    //b=Math.trunc(Math.max(100) * 256),
+    //c=Math.trunc(Math.random() * 256),color;
+    //color = "rgb(" +a+ "," +b+ "," +c+ ")";
+    //document.body.style.background=color;
+	//console.log(color);
+//}
+
+//background();
+
+//vowels
+//let
+//vow1=prompt("Give first word"),
+//vow2=prompt("Give second word"),
+//vowels=["a","e","i","o","u"],
+//a=0,b=0,i=0,j=0;
+
+//for(i=0;i<vow1.length;i++){
+//	for(j=0;j<vowels.length;j++){
+//		if(vow1[i]==vowels[j]){
+//			a++;
+//		}
+//	}
+//}
+
+//for(i=0;i<vow2.length;i++){
+//	for(j=0;j<vowels.length;j++){
+//		if(vow2[i]==vowels[j]){
+//			b++;
+//		}
+//	}
+//}
+//if(a==b){
+	//console.log("Vowels are equal");
+//}
+
+//else if(a>b){
+	//console.log("First word has More vowels",a);
+//}
+//else{
+	//console.log("Second word has More,voewls",b);
+//}
+
+
+
+//let string=parseInt(prompt("enter the number")),rem,a=[],i=0,
+//str=["zero","one","two","three","four","five","six","seven","eight","nine"];
+//while(string>0) {
+//  string=parseInt(string);
+//   rem=string%10;
+//    string=string/10;
+//	i=rem;
+//	if(rem>0){
+//	console.log("");
+//    console.log(str[i]);
+//	}
+//}
+//let n=[10,6,11,20,12],arr=[],i=0,j=0;
+//for(i=0;i<n.length;i++){
+//	for(j=0;j<n.length;j++){
+//		if(n[i]<arr[j]){
+//			console.log(n[j]);
+//			break;
+//		}
+//	}
+//}
+
+// function smaller(post){
+//    post=post.sort(function(a,b){return a-b})
+//    if(post==""){
+//   console.log("-1");
+//    }
+//    else
+//        console.log(post[0]);
+
+// }
+
+// let a=[20,40,60,-20,12],post=[];
+// for(i=0;i<a.length;i++){
+//    for(j=0;j<a.length;j++){
+//        if(a[i]<a[j]){
+//            post.push(a[j]);
+//        }
+        
+//    }
+//    smaller (post);
+//    post=[];
+// }
+
+// let
+// date=new Date(),
+// day=date.getDay();
+// //console.log(day);
+
+// if(day==0||day==6){
+// 	console.log("Tha Day is Week End")
+// 	}
+	
+// else{
+// 	console.log("The Day is not weekend")
+// }
+
+//let 
+//dob,time,today,age,year,total_yr;
+//dob=new Date("07/30/1996");
+//time=dob.getTime();
+//today=Date.now()-time;
+//age=new Date(today);
+//year=age.getFullYear();
+//total_yr=Math.abs(year-1970);
+//console.log("The Age is",total_yr);
+
+
+
+//1st program
+
+/*  let d = new Date(2022, 09, 15, 10, 30, 30, 0);
+ let d2 = new Date(2022, 09, 16, 12, 30, 30, 0);
+ let date=d-d2;
+ date=Math.abs(date);
+ date=date/(1000*60*60);
+ date=Math.floor(date);
+console.log("The difference in"+date+"hour"); */
+
+ /*function background(){
+   let 
+   a=Math.trunc(Math.random()*256),
+   b=Math.trunc(Math.random()*256),
+ c=Math.trunc(Math.random()*256),color;
+
+  color="rgb("+a+","+b+","+c+")";
+ //  console.log(color);
+   document.body.style.background=color;
+ }
+document.getElementById("btn").addEventListener("click",background);*/
+
+//function bg_color(){
+  //let 
+  //color=document.getElementById("change").value;
+
+  //document.body.style.background=color
+//}
+//document.getElementById("btn").addEventListener("click",bg_color);
+/* 
+ let 
+ a="leve",b;
+ b=a.split("");
+ b=b.reverse();
+ b=b.join("");
+console.log(b);
+
+ 	if(a==b){
+ 		console.log("This is Polyndrome");
+	}
+ 	else{
+ 		console.log("Not Polyndrome");
+	} */
+
+/* let d=21,mon=09,yr=2022;
+date=mon+"/"+d+"/"+yr;
+//console.log(date);
+let new_date=new Date(date);
+let c_day=new_date.getDay(new_date);
+output=new_date-((24*60*60*1000)*(c_day-1));
+output=new Date(output);
+
+output=
+console.log(output); */
+
+
+
+
+function num(){
+	
+    let number=document.getElementById("change").value,rem,sum=0,i=0;
+    str=["zero","one","two","three","four","five","six","seven","eight","nine"];
+    while(number>0){
+    
+      rem=number%10;
+      sum=sum*10+rem;
+      number=parseInt(number/10);
+	  
+	  if(rem<0){
+		  console.log(srt[i]);
+	  }
+    }
+	document.getElementById("ans").innerHTML="Reverse of a Number is "+sum;
+	//console.log(sum);
+}
+document.getElementById("btn").addEventListener("click",num);
+
+
+/* let a ="abcd#21@";
+let b=a.replace("@","");
+console.log(b); */
+/*  let string=prompt("Give the word");
+function remove(){
+	let b=string.replace(/[^a-zA-Z0-9]/g, "");
+	console.log(b);
+}
+remove(); */
+// let string=prompt("Give the word");
+/*  let string="karthik@#123";
+function remove(){
+	let letter=string.replace(/[^a-zA-Z]/g, ""),
+	num=string.replace(/[^0-9]/g, "");
+	console.log(letter);
+	console.log(num);
+	
+}
+remove(); */
+
+let numbers="123457689",
+odd,even;
+even=numbers.replace(/[^24680]/g,"");
+console.log("The even Numbers are "+even);
+odd=numbers.replace(/[^13579]/g,"");
+console.log("The odd Numbers are "+odd);
